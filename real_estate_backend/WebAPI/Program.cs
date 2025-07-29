812b4a46787643afc3a28c0c28882f98f1e70e35
mihajlogalicki <mihajloo966@gmail.com>
2025-07-29 21:48:16 +0200

    Added CORS rules with allowed origin and specific HTTP methods

--- real_estate_backend/WebAPI/Program.cs
+++ real_estate_backend/WebAPI/Program.cs
@@ -1,12 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var myAllowedOrigin = "_myAllowedResourceShareOrigin";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowedOrigin,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .WithMethods("GET", "POST", "PUT");
+        });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(myAllowedOrigin);
 
 app.UseAuthorization();
 
