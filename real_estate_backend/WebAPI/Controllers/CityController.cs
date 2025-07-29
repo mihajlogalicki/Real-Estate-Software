2b9a82ceb01a9b9cf9deef2eca3d79ede3152a49
mihajlogalicki <mihajloo966@gmail.com>
2025-07-29 04:18:40 -0700

    Preparing GET Api for city controller

---
+++ real_estate_backend/WebAPI/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> City()
+        {
+            return new string[] { "Belgrade", "Paris", "Madrid" };
+        }
+
+        [HttpGet("{id}")]
+        public string City(int id)
+        {
+            return "Belgrade";
+        }
+    }
+}
