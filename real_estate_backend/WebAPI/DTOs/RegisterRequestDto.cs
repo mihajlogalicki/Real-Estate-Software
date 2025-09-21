1da8dcb942231dfa6c038ac72620c8d7b412e4c1
mihajlogalicki <mihajloo966@gmail.com>
2025-09-21 20:05:17 +0200

    Finishing back-end custom validation for user Registration functionality, added Reg Expression validation for email, mobile and password.

--- real_estate_backend/WebAPI/DTOs/RegisterRequestDto.cs
+++ real_estate_backend/WebAPI/DTOs/RegisterRequestDto.cs
@@ -5,12 +5,19 @@ namespace WebAPI.DTOs
     public class RegisterRequestDto
     {
         [Required]
+        [StringLength(15, MinimumLength = 5)]
         public string Username { get; set; }
+
         [Required]
+        [RegularExpression(@"^(?=(.*\d))(?=(.*[a-zA-Z]))(?=(.*[\W_]))[a-zA-Z\d\W_]{7,20}$", ErrorMessage="Password is not valid.")]
         public string Password { get; set; }
+
         [Required]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com|outlook\.com)$", ErrorMessage = "Email is not valid.")]
         public string Email { get; set; }
+
         [Required]
+        [RegularExpression(@"^\+([1-9]\d{1,4})[\s\(\)-]?\d{1,4}[\s\(\)-]?\d{1,4}[\s\(\)-]?\d{1,4}$", ErrorMessage = "Mobile phone is not valid.")]
         public string Mobile { get; set; }
     }
 }
