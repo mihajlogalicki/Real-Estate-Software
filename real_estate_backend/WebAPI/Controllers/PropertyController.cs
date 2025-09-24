95bcb910d9381745408205eda721a64761cb75fb
mihajlogalicki <mihajloo966@gmail.com>
2025-09-24 19:18:34 +0200

    Preparing PropertyController with Authorization (in-progress).

---
+++ real_estate_backend/WebAPI/Controllers/PropertyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Interfaces;
+
+namespace WebAPI.Controllers
+{
+    [Authorize]
+    public class PropertyController : BaseController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PropertyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("type/{sellRentType}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetPropertyList(int sellRentType)
+        {
+            var properties = await _unitOfWork.PropertyRepository.GetPropertiesAsync(sellRentType);
+            return Ok(properties);
+        }
+    }
+}
